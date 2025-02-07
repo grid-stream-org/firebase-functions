0fb48fda6e5417b2e8c2b5ee726cf1ecc1a8c497
matthew-collett <matthew.collett12@gmail.com>
2025-02-07 11:10:42 -0400

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const { onCall } = require('firebase-functions/v2/https')
+const { initializeApp } = require('firebase-admin/app')
+const { getAuth } = require('firebase-admin/auth')
+
+initializeApp()
+
+exports.getUserEmailById = onCall(async request => {
+  const userId = request.data.userId
+
+  if (!userId) {
+    throw new Error('User ID is required')
+  }
+
+  try {
+    const userRecord = await getAuth().getUser(userId)
+    return { email: userRecord.email }
+  } catch (error) {
+    throw new Error(`Error retrieving user email: ${error.message}`)
+  }
+})
